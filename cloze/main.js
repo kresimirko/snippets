19dbf828ef9e3e905589c49cafa1882307412293
Luka K. Mihovilović <lukak.business@gmail.com>
2025-12-29 16:21:44 +0100

    cLoze - add marked for md support

--- cloze/main.js
+++ cloze/main.js
@@ -1,9 +1,13 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+
 console.log("started");
 
 const defaultIntensity = 8;
+const defaultIgnoreHeadings = true;
 
 const lText = localStorage.getItem("lText");
 const lIntensity = localStorage.getItem("lIntensity") ?? defaultIntensity;
+const lIgnoreHeadings = localStorage.getItem("lIgnoreHeadings") ?? defaultIgnoreHeadings;
 
 const actualClozeArea = document.getElementById("actualClozeArea");
 const resetBtn = document.getElementById("resetBtn");
@@ -18,14 +22,18 @@ const clozeFinishBtn = document.getElementById("clozeFinishBtn");
 const clozeShowBtn = document.getElementById("clozeShowBtn");
 const clozeClearBtn = document.getElementById("clozeClearBtn");
 const counterE = document.getElementById("counter");
+const ignoreHeadingsCheckbox = document.getElementById("ignoreHeadings");
 
 // ! currently unused
 // TODO: add more punc. signs
 /* const pre = ["(", "[", "{"];
 const post = [".", ",", ":", ";", "-", ")", "}", "]", "\u2013", "\u2014"]; */
 
-intensityInput.value = lIntensity ?? defaultIntensity;
+const an = "1234567890abcdefghijklmnopqrstuvwxyzšđčćž".split("");
+
+intensityInput.value = lIntensity;
 intensityPercentage.innerText = intensityInput.value + "%";
+ignoreHeadingsCheckbox.checked = lIgnoreHeadings === "true";
 
 resetBtn.addEventListener("click", () => {
     localStorage.setItem("lText", "");
@@ -33,6 +41,11 @@ resetBtn.addEventListener("click", () => {
 });
 setIntensityBtn.addEventListener("click", () => {
     localStorage.setItem("lIntensity", intensityInput.value);
+    location.reload();
+});
+ignoreHeadingsCheckbox.addEventListener("click", () => {
+    console.log(ignoreHeadingsCheckbox.checked);
+    localStorage.setItem("lIgnoreHeadings", ignoreHeadingsCheckbox.checked);
     location.reload();
 });
 clozeFinishBtn.addEventListener("click", finish);
@@ -55,21 +68,43 @@ function begin(value) {
     enterContainer.style.display = "none";
     clozeContainer.style.display = "";
 
-    let textArray = value.split(" ");
+    let charArray = value.split("");
 
-    for (const word of textArray) {
-        const roll = Math.floor((Math.random() * 1) / (lIntensity / 100));
-        if (roll !== 0) {
-            const newE = document.createElement("span");
-            newE.innerText = word;
-            actualClozeArea.appendChild(newE);
-        } else {
-            const newE = document.createElement("input");
-            newE.type = "text";
-            newE.setAttribute("solution", word);
-            actualClozeArea.appendChild(newE);
+    let currentCharIsAlNum = false;
+    let isInHeading = false;
+    let fullMd = "";
+    let nextWord = "";
+    for (const char of charArray) {
+        if (char === "#")
+            isInHeading = true;
+        if (char === "\n")
+            isInHeading = false;
+        if (isInHeading && ignoreHeadingsCheckbox.checked) {
+            fullMd += char;
+            continue;
         }
+        
+        currentCharIsAlNum = an.includes(char.toLowerCase());
+
+        if (!currentCharIsAlNum) {
+            if (nextWord.length > 0) {
+                const roll = Math.floor((Math.random() * 1) / (lIntensity / 100));
+                if (roll === 0)
+                    fullMd += `<input type="text" solution="${nextWord}" />`;
+                else
+                    fullMd += nextWord;
+            }
+            nextWord = "";
+            fullMd += char;
+            continue;
+        }
+
+        nextWord += char;
     }
+
+    console.log(fullMd);
+    const parsed = marked.parse(fullMd);
+    actualClozeArea.innerHTML = parsed;
 }
 
 function finish() {
